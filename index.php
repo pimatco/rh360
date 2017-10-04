faf0eb0689247725daceeaddd00e35d8ead167c9
pimatco <pimatco@gmail.com>
2017-10-04 20:25:37 -0300

    - added favicon/ -small changes to appearance on index, postagem1, postagem2 e postagem3 para ficarem prontos para publicar.

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ $user_ip = getUserIP();
 <head>
 	<title>RH 360</title>
 	<meta charset="utf-8" />
+	<link rel="icon" type="image/png" href="images/favicon.png">
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" href="assets/css/main.css" />
@@ -36,17 +37,17 @@ $user_ip = getUserIP();
 	<!--Banner principal-->
 	<div id="titulo-principal" class="container">
 		<h1 id="titulo-principal">
-			<a href="index.html">
-				<font id="logo">RH</font> 360
+			<a href="index.php">
+				<img src="images/logo-cabecalho.png" height="133" width="184">
 			</a>
 		</h1>
 	</div>
 	<header id="inicial">
 		<div class="container TopCasaFina-bannerWrapper">
 			<div class="TopCasaFina-banner">
-				<h1>Milky Way Tour by Camping on Mount Bromo</h1>
-				<p class="data-postagem">in Travel / 26.12.2016 <p>
-					<a class="btn btn-success" href="#" role="button">read more</a>
+				<h1>Tudo sobre Recursos Humanos a um clique</h1>
+				<p class="data-postagem">01.10.2017 <p>
+					<a class="btn btn-success" href="#" role="button">Confira nosso post mais lido</a>
 			</div>
 		</div>
 	</header>
@@ -103,9 +104,6 @@ $user_ip = getUserIP();
 						<li><a href="postagens/postagem1.html" class="button big">Continuar lendo</a></li>
 					</ul>
 					<ul class="stats">
-						<li><a href="#">General</a></li>
-						<li><a href="#" class="icon fa-heart">28</a></li>
-						<li><a href="#" class="icon fa-comment">128</a></li>
 					</ul>
 				</footer>
 			</article>
@@ -123,10 +121,10 @@ $user_ip = getUserIP();
 
 				<!-- Intro -->
 				<section id="intro">
-					<a href="#" class="logo"><img src="images/logo.jpg" alt="" /></a>
+					<a href="#" class="logo"><img src="images/profile.png" alt="" /></a>
 					<header>
-						<h2>Future Imperfect</h2>
-						<p>Another fine responsive site template by <a href="http://html5up.net">HTML5 UP</a></p>
+						<h2>RH360</h2>
+						<p>Tudo sobre Recursos Humanos em apenas um lugar.</a></p>
 					</header>
 				</section>
 
@@ -138,7 +136,7 @@ $user_ip = getUserIP();
 						<article class="mini-post">
 							<header>
 								<h3><a href="postagens/postagem1.html">A GESTÃO DE PESSOAS ATRAVÉS DO PERFIL COMPORTAMENTAL</a></h3>
-								<time class="published" datetime="2015-10-20">Outubro 1, 2017</time>
+								<time class="published" datetime="2015-10-20">1 de Outubro de 2017</time>
 								<a href="https://www.linkedin.com/in/let%C3%ADcia-pegoraro-07272814a/" target="_blank" class="author"><img src="images/Leticia Pegoraro.jpg" alt="Leticia Pegoraro" /></a>
 							</header>
 							<a href="postagens/postagem1.html" class="image"><img src="images/post-1-perfil-comportamental.png" alt="" /></a>
@@ -148,7 +146,7 @@ $user_ip = getUserIP();
 						<article class="mini-post">
 							<header>
 								<h3><a href="postagens/postagem2.html">Como aumentar a produtividade com a gestão comportamental</a></h3>
-								<time class="published" datetime="2015-10-20">Outubro 1, 2017</time>
+								<time class="published" datetime="2015-10-20">2 de Outubro de 2017</time>
 								<a href="https://www.linkedin.com/in/let%C3%ADcia-pegoraro-07272814a/" target="_blank" class="author"><img src="images/Leticia Pegoraro.jpg" alt="Leticia Pegoraro" /></a>
 							</header>
 							<a href="postagens/postagem2.html" class="image"><img src="images/post-2-aumento-produtividade.png" alt="" /></a>
@@ -157,8 +155,8 @@ $user_ip = getUserIP();
 						<!-- Mini Post -->
 						<article class="mini-post">
 							<header>
-								<h3><a href="postagens/postagem3.html">5 - ARGUMENTOS PARA IMPACTAR GESTORES DA IMPORTÂNCIA DO SOFTWARE DE RH</a></h3>
-								<time class="published" datetime="2015-10-20">Outubro 1, 2017</time>
+								<h3><a href="postagens/postagem3.html">5 ARGUMENTOS PARA IMPACTAR GESTORES DA IMPORTÂNCIA DO SOFTWARE DE RH</a></h3>
+								<time class="published" datetime="2015-10-20">3 de Outubro de 2017</time>
 								<a href="https://www.linkedin.com/in/andi-cris-souza-santana-a245aa95/" target="_blank" class="author"><img src="images/Andi Cris Souza.jpg" alt="" /></a>
 							</header>
 							<a href="postagens/postagem3.html" class="image"><img src="images/post-3-software-rh.png" alt="" /></a>
@@ -167,21 +165,22 @@ $user_ip = getUserIP();
 				</section>
 				<!-- About -->
 				<section class="blurb">
-					<h2>About</h2>
-					<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod amet placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at phasellus sed ultricies.</p>
+					<h2>Sobre nós</h2>
+					<p align="justify">Somos um blog de RH para empresas de todos os portes e segmentos, que queiram aumentar sua produtividade formando equipes de alta performance. Nosso diferencial é oferecer ferramentas práticas e gratuitas para melhorar o recrutamento e alocação dos seus colaboradores. Acreditamos que o RH estratégico reduz rotatividade e custos desnecessários com demissões, recontratações e novos treinamentos. Podemos te ajudar nessa tarefa! Cada profissional se encaixa em um perfil e cada um desempenha melhor certas atividades. Siga nossa página, faça o teste GRATUITO de perfis e melhore agora o seu time! </p>
 					<ul class="actions">
-						<li><a href="#" class="button">Learn More</a></li>
+						<li><a href="postagens/postagem1.html" class="button">Acesse nosso post mais visitado</a></li>
 					</ul>
 				</section>
 
 				<!-- Footer -->
 				<section id="footer">
 					<ul class="icons">
-						<li><a href="#" class="fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="fa-facebook"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="fa-instagram"><span class="label">Instagram</span></a></li>
+					<!--<li><a href="www.twitter.com/" class="fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="www.facebook.com/" class="fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="www.instagram.com/" class="fa-instagram"><span class="label">Instagram</span></a></li>
 						<li><a href="#" class="fa-rss"><span class="label">RSS</span></a></li>
-						<li><a href="#" class="fa-envelope"><span class="label">Email</span></a></li>
+					-->
+						<li><a href="mailto:contato@rh360.com.br" class="fa-envelope"><span class="label">Email</span></a></li>
 					</ul>
 				</section>
 			</section>
